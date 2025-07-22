665ad45516f5ef3f5207bd083f82b8c8583928b7
ginsum-dev <jennyjung.dev@gmail.com>
2025-07-22 23:19:51 +0900

    Feat: TodoItem 추가

---
+++ src/components/TodoItem.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+let { todo, toggleTodo, deleteTodo, i  } = $props();
+</script>
+
+<li>
+    <input type="checkbox" checked={todo.done} onchange={() => toggleTodo(i)} />
+    <span style="text-decoration: {todo.done ? 'line-through' : 'none'}">
+      {todo.text}
+    </span>
+    <button onclick={() => deleteTodo(i)}>삭제</button>
+</li>
